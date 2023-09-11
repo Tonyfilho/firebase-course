39bf07ab25cbe0a09d61fc2cc59b6e416e768358
tonyfilho <oficinadosdrones@gmail.com>
2023-09-11 21:38:37 +0100

    courses.service.ts

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,9 +1,10 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {ICourse} from '../model/course';
-import {finalize, tap} from 'rxjs/operators';
-import {Observable} from 'rxjs';
-import {ILesson} from '../model/lesson';
+import { CoursesService } from './../services/courses.service';
+import { AfterViewInit, Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ICourse } from '../model/course';
+import { finalize, tap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { ILesson } from '../model/lesson';
 
 
 @Component({
@@ -11,20 +12,40 @@ import {ILesson} from '../model/lesson';
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent implements OnInit, OnDestroy {
 
   loading = false;
-
+  lastPageLoaded = 0;  /** this is firstPage will get load, start with zero*/
   displayedColumns = ['seqNo', 'description', 'duration'];
   course!: ICourse;
-  
-  constructor(private route: ActivatedRoute) {
+  lessons!: ILesson[];
+  subs: Subscription;
+
+  constructor(private route: ActivatedRoute, private couserService: CoursesService) {
   }
-  
   ngOnInit() {
+    this.loading = true;
     /**Get the value via RESOLVER SERVICES */
     this.course = this.route.snapshot.data["course"];
-    
+    this.subs = this.couserService.findLessons(this.course.id)
+      .pipe(
+        /**when the Observable Finalize, or Success or Error the Finalize will call */
+        finalize(() => this.loading = false)
+      )
+      .subscribe({
+        next: (data: ILesson[]) => this.lessons = data,
+        error: err => { },
+        complete: () => { }
+      }
+      );
+
+  }
+  loadMore() {
+    /**Paginator */
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,3 +1,6 @@
+
+
+
 import { Observable, from } from 'rxjs';
 import { Injectable } from '@angular/core';
 
@@ -6,6 +9,12 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { map, concatMap } from 'rxjs/operators';
 import { convertSnap } from './db-converter-types-util';
 import { ILesson } from '../model/lesson';
+
+/**Note: The Way of import, is different*/
+import firebase from 'firebase';
+import OrderByDirection = firebase.firestore.OrderByDirection;
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,29 +27,56 @@ export class CoursesService {
 
 
 
+  /*** Find Lesson with Paginator */
+  /**Will return a List of Lesson but with paginator */
+
+  findLessons(courseID: string, sortOrder: OrderByDirection = 'asc', pageNumber = 0, pageSize = 3): Observable<ILesson[]> {
+    /**1º Path list of Lessons */
+    /**2º Ref ordeBy the ASC */
+  return  this.db.collection(`courses/${courseID}/lessons`,
+    /**3º Limit of the data per page */ 
+    ref => ref.orderBy("segNo", sortOrder).limit(pageSize)
+    /**4º Pagenator quantity*/
+    .startAfter(pageNumber * pageSize)    
+    )
+    /**5º Grab the Get will return a Observable */
+    .get()
+    /**6º We are going to get a list of query results, and use MAP() Rxjs, we are going to MAP the Observable of the courses or Null*/
+    .pipe(
+      map(result => convertSnap(result))
+    );
+
+    
+
+  }
+
+
+
+
+
 
   /*** Find By URL */
   /**Will return only on data in database */
   findCourseByUrl(courseURL: string): Observable<ICourse | null> {
     /**1º Get all the collection and created a REF with where is property is same of courseURL */
-  return  this.db.collection("courses", ref => ref.where("url", "==", courseURL))
+    return this.db.collection("courses", ref => ref.where("url", "==", courseURL))
       /**2º grab the get Observable, as usual to get current results */
       .get()
       .pipe(
-        /**3º We are going to get a list of query results, and use MAP() Rxjs, we agoing to MAP the Observable of the courses or Null*/
+        /**3º We are going to get a list of query results, and use MAP() Rxjs, we are going to MAP the Observable of the courses or Null*/
         map(result => {
           console.log("Results.doc :", result.docs);
-          /**4º We are still goibg to get here an Array back as a results, we need to convert it */
-          result.docs.map(dataResults => {console.log("list dataResults.data(): ",  dataResults.data() )});
+          /**4º We are still going to get here an Array back as a results, we need to convert it */
+          result.docs.map(dataResults => { console.log("list dataResults.data(): ", dataResults.data()) });
           const localCourses: ICourse[] = convertSnap<ICourse>(result);
           /**5º If the courses results is 1 value,is right, or other hand we not find any results  or if find multiples results, then the results will be Null
            * or ather hand our query is invalid. */
-         return  localCourses.length == 1 ? localCourses[0] : null;
+          return localCourses.length == 1 ? localCourses[0] : null;
         })
         /**6º Now we  need to configure this as RESOLVER in you routeModule */
 
       );
-    
+
   }
 
 
