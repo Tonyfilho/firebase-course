9a9bd0ef5081d0bd598a7a3cc93594cb17a02eae
tonyfilho <oficinadosdrones@gmail.com>
2023-09-13 15:44:19 +0100

    singOut singIn

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -18,7 +18,7 @@ export class UsersService {
   constructor(private afAuth: AngularFireAuth, private router: Router) {   
     this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggeIn => !loggeIn));  
-    this.pictureUrl$ = this.afAuth.authState.pipe(map(userInfo => userInfo? userInfo.photoURL: null));
+    this.pictureUrl$ = this.afAuth.authState.pipe(map(userInfo => userInfo ? userInfo.photoURL: null));
     /**_Let`s consume this observables and show or hide certain elements of the user interface, let`s to AppComponent */   
     
    }
