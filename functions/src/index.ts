966ee6e9cfc3afed58b57193226e93513273be38
tony_filho <antonio.filho@nexllence.com>
2023-10-19 09:54:04 +0100

    erro at import from firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,14 +6,15 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+import * as functions from "firebase-functions";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+     functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
