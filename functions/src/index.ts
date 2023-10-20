9f30d496c192213b144d08626113cd0cd97c31c1
tonyfilho <oficinadosdrones@gmail.com>
2023-10-20 11:12:29 +0100

    firebase Cloud Function hellow word

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,7 +14,9 @@ import * as functions from "firebase-functions";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+
+
+export const helloWorld = functions.https.onRequest((_request, _response) => {
      functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  _response.send("Hello from Firebase!");
 });
